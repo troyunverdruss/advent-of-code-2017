888a47346601d14f65049b3aecf5a875c59d203d
Troy Unverdruss <troy@unverdruss.net>
2019-03-18 20:03:21 -0700

    Puzzle 15

--- days/day15/puzzle_15.py
+++ days/day15/puzzle_15.py
@@ -1,6 +1,68 @@
-def solve_15():
-    pass
+import sys
+
+sys.path.append('/Users/troy/Documents/code/advent-of-code-2017')
+from helpers import read_raw_entries
+
+
+class Generator:
+    def __init__(self, seed, factor, match_multiple=1):
+        self.value = seed
+        self.factor = factor
+        self.match_multiple = match_multiple
+
+    def next(self):
+        # Part 1
+        if self.match_multiple == 1:
+            _next = self.value * self.factor % 2147483647
+            self.value = _next
+            return _next
+
+        # Part 2
+        _next = self.value * self.factor % 2147483647
+        while _next % self.match_multiple != 0:
+            self.value = _next
+            _next = self.value * self.factor % 2147483647
+        self.value = _next
+        return _next
+
+
+def lower_16(i):
+    return bin(i)[2:].zfill(16)[-16:]
+
+
+def solve_15(seed_a, seed_b):
+    # generator_a = Generator(65, 16807)
+    # generator_b = Generator(8921, 48271)
+    generator_a = Generator(seed_a, 16807)
+    generator_b = Generator(seed_b, 48271)
+
+    matches = 0
+    for i in range(40_000_000):
+        if lower_16(generator_a.next()) == lower_16(generator_b.next()):
+            matches += 1
+
+    return matches
+
+
+def solve_15b(seed_a, seed_b):
+    generator_a = Generator(seed_a, 16807, 4)
+    generator_b = Generator(seed_b, 48271, 8)
+
+    matches = 0
+    for i in range(5_000_000):
+        if lower_16(generator_a.next()) == lower_16(generator_b.next()):
+            matches += 1
+
+    return matches
 
 
 if __name__ == '__main__':
-    r = solve_15()
+    entries = read_raw_entries('input_15.txt')
+    seed_a = int(entries[0].split()[-1])
+    seed_b = int(entries[1].split()[-1])
+    r = solve_15(seed_a, seed_b)
+    print('part 1, 40M cycles, matched: {}'.format(r))
+    # 592
+
+    r = solve_15b(seed_a, seed_b)
+    print('part 2, 5M cycles, matched: {}'.format(r))
--- tests/day15/test_15.py
+++ tests/day15/test_15.py
@@ -1,5 +1,37 @@
 from unittest import TestCase
 
+from days.day15.puzzle_15 import Generator
+
 
 class TestSolve15(TestCase):
-    pass
+    def test_gen_a(self):
+        gen = Generator(65, 16807)
+        self.assertEqual(1092455, gen.next())
+        self.assertEqual(1181022009, gen.next())
+        self.assertEqual(245556042, gen.next())
+        self.assertEqual(1744312007, gen.next())
+        self.assertEqual(1352636452, gen.next())
+
+    def test_gen_b(self):
+        gen = Generator(8921, 48271)
+        self.assertEqual(430625591, gen.next())
+        self.assertEqual(1233683848, gen.next())
+        self.assertEqual(1431495498, gen.next())
+        self.assertEqual(137874439, gen.next())
+        self.assertEqual(285222916, gen.next())
+
+    def test_gen_a_part_2(self):
+        gen = Generator(65, 16807, 4)
+        self.assertEqual(1352636452, gen.next())
+        self.assertEqual(1992081072, gen.next())
+        self.assertEqual(530830436, gen.next())
+        self.assertEqual(1980017072, gen.next())
+        self.assertEqual(740335192, gen.next())
+
+    def test_gen_b_part_2(self):
+        gen = Generator(8921, 48271, 8)
+        self.assertEqual(1233683848, gen.next())
+        self.assertEqual(862516352, gen.next())
+        self.assertEqual(1159784568, gen.next())
+        self.assertEqual(1616057672, gen.next())
+        self.assertEqual(412269392, gen.next())
